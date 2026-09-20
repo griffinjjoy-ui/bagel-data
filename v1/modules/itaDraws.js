7dbe34221e9b9167dc26b7f865d1ec07973867ad
bagel-actions <actions@users.noreply.github.com>
2026-09-20 05:10:58 +0000

    data 2026-09-20 (8a9547a3ab)

--- v1/modules/itaDraws.js
+++ v1/modules/itaDraws.js
@@ -9,7 +9,7 @@ export const itaDraws = globalThis.__BAGEL_REMOTE__?.["itaDraws"]?.itaDraws ?? {
   "name": "2026 ITA Men's All American Championships",
   "gender": "men",
   "tz": null,
-  "fetchedAt": "2026-09-20T04:47:03.162Z",
+  "fetchedAt": "2026-09-20T05:10:51.110Z",
   "events": [
    {
     "eventId": "1D12B806-D80E-4B5A-A27F-11A9672F397C",
@@ -11808,7 +11808,7 @@ export const itaDraws = globalThis.__BAGEL_REMOTE__?.["itaDraws"]?.itaDraws ?? {
   "name": "2026 ITA Women's All American Championships",
   "gender": "women",
   "tz": "ET",
-  "fetchedAt": "2026-09-20T04:47:05.826Z",
+  "fetchedAt": "2026-09-20T05:10:54.207Z",
   "events": [
    {
     "eventId": "8E1C0034-A750-47A3-AB61-45CE8BCA7DBE",
